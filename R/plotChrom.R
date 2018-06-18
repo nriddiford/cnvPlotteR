8b5053aa455310671c8c0971be6640498df325c7
nriddiford <nickriddiford@me.com>
2018-06-18 17:45:38 +0200

    Adjust for dark background

--- R/plotChrom.R
+++ R/plotChrom.R
@@ -1,5 +1,3 @@
-#' chromPlot
-#'
 #' Function to plot a single chromosomes for a given sample
 #' @param chrom Specify the chromosome to plot [Defualt 'X']
 #' @param cnv_file File to plot. [Required]
@@ -10,9 +8,8 @@
 #' @keywords plot chrom
 #' @export
 #' @examples chromPlot(cnv_file = "data/test.window-10000.cnv", chrom = "3R", ylim=c(-6,6))
-
-
-chromPlot <- function(chrom = NA, cnv_file, ylim=c(-5,5), ext='png', write=TRUE) {
+chromPlot <- function(cnv_file, chrom = NA, ylim=c(-5,5), ext='png',
+                      theme = 'white', write=TRUE) {
 
   cat("Processing", cnv_file, "\n")
 
@@ -34,12 +31,22 @@ chromPlot <- function(chrom = NA, cnv_file, ylim=c(-5,5), ext='png', write=TRUE)
   read_file_in <- read.delim(cnv_file, header = T)
   clean_file <- cleanR(read_file_in)
 
-  # cols <- brewer.pal(n = 7, name = "RdBu")
-  cols <- c("#941212FE", "#C44747FE", "#B3A5A5FE", "#4FA9BDFE", "#248DB3FE")
-
+  # For white background
+  if(theme=='white'){
+    cols <- c("#941212FE", "#C44747FE", "#B3A5A5FE", "#4FA9BDFE", "#248DB3FE")
+    customTheme = cleanTheme()
+  } else if(theme=='black'){ # For black background
+    # cols <- c("#EB7609", "#EBA17C", "#B3A5A5FE", "#4FA9BDFE", "#248DB3FE")
+    cols <- c("#F5D520", "#EBDC86", "#BCC1CC", "#9ACAD6", "#4CA8E6")
+    customTheme = blackTheme()
+    ext <- paste0('_dark.', ext)
+  } else{
+    cat(theme, " not a valid theme. Please use 'white' or 'black'", "\n")
+    break
+  }
   chrom_data <- filter(clean_file, chromosome == chrom)
 
-  p <- ggplot(data=chrom_data, aes(start/1000000, log2, colour = log2))
+  p <- ggplot(chrom_data, aes(start/1000000, log2, colour = log2))
   p <- p + geom_point()
   p <- p + scale_alpha(range = c(0.1, 5))
   p <- p + scale_x_continuous("Mb", expand = c(0.001, 0.001), breaks = seq(0, (max(chrom_data$end)-1), by = 1))
@@ -47,21 +54,11 @@ chromPlot <- function(chrom = NA, cnv_file, ylim=c(-5,5), ext='png', write=TRUE)
   # p <- p + scale_colour_gradientn(colours = cols, values = rescale(c(-3, -0.585, -0.001, 0, 0.001, 0.585, 3)), guide = "colorbar", limits = ylim)
   p <- p + scale_colour_gradientn(colours = cols, values = rescale(c(-3, -0.25, 0, 0.25, 3)), guide = "colorbar", limits = ylim)
 
-  # p <- p + blackTheme()
-  p <- p + cleanTheme()
+  p <- p + customTheme
 
   if(chrom == "X"){
     p <- p + geom_vline(xintercept = 3.134, colour="slateblue", alpha=.7, linetype="dotted") + geom_vline(xintercept = 3.172, colour="slateblue", alpha=.7, linetype="dotted")
   }
-  else {
-    p <- p + geom_hline(yintercept = -0.322, colour = "slateblue", alpha = 0.4, linetype = "dotted")
-    p <- p + geom_hline(yintercept = 0.322, colour = "slateblue", alpha = 0.4, linetype = "dotted")
-    p <- p + geom_hline(yintercept = -0.585, colour = "blue", alpha = 0.4)
-    p <- p + geom_hline(yintercept = 0.585, colour = "blue", alpha = 0.4)
-  }
-
-  # p <- p + geom_hline(yintercept = -1, colour = "royalblue", alpha = 0.6)
-  # p <- p + geom_hline(yintercept = 1, colour = "royalblue", alpha = 0.6)
 
   p <- p + ggtitle(paste(sample, " ", chrom, sep = ""))
 
@@ -71,5 +68,4 @@ chromPlot <- function(chrom = NA, cnv_file, ylim=c(-5,5), ext='png', write=TRUE)
     ggsave(paste("plots/chroms/", outfile, sep = ""), width = 20, height = 10)
   }
   p
-
 }
